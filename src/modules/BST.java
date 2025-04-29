0b377dab3283776413aa9c791d599359627cdc58
Abdunur <amangeldievabdunur@gmail.com>
2025-04-29 22:16:31 +0600

    updated bst class

--- src/modules/BST.java
+++ src/modules/BST.java
@@ -1,9 +1,22 @@
 package modules;
-
 import java.util.*;
 
-public class BST<K extends Comparable<K>, V> {
+public class BST<K extends Comparable<K>, V> implements Iterable<BST.Entry<K, V>> {
     private Node root;
+    private int size = 0;
+
+    public static class Entry<K, V> {
+        private final K key;
+        private final V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() { return key; }
+        public V getValue() { return value; }
+    }
 
     private class Node {
         private K key;
@@ -16,13 +29,19 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
+    public int size() {
+        return size;
+    }
+
     public void put(K key, V val) {
         root = put(root, key, val);
     }
 
     private Node put(Node node, K key, V val) {
-        if (node == null) return new Node(key, val);
-
+        if (node == null) {
+            size++;
+            return new Node(key, val);
+        }
         int cmp = key.compareTo(node.key);
         if (cmp < 0)
             node.left = put(node.left, key, val);
@@ -30,7 +49,6 @@ public class BST<K extends Comparable<K>, V> {
             node.right = put(node.right, key, val);
         else
             node.val = val;
-
         return node;
     }
 
@@ -40,7 +58,6 @@ public class BST<K extends Comparable<K>, V> {
 
     private V get(Node node, K key) {
         if (node == null) return null;
-
         int cmp = key.compareTo(node.key);
         if (cmp < 0)
             return get(node.left, key);
@@ -56,13 +73,13 @@ public class BST<K extends Comparable<K>, V> {
 
     private Node delete(Node node, K key) {
         if (node == null) return null;
-
         int cmp = key.compareTo(node.key);
         if (cmp < 0)
             node.left = delete(node.left, key);
         else if (cmp > 0)
             node.right = delete(node.right, key);
         else {
+            size--;
             if (node.left == null) return node.right;
             if (node.right == null) return node.left;
 
@@ -71,7 +88,6 @@ public class BST<K extends Comparable<K>, V> {
             node.val = min.val;
             node.right = delete(node.right, min.key);
         }
-
         return node;
     }
 
@@ -80,16 +96,35 @@ public class BST<K extends Comparable<K>, V> {
         return node;
     }
 
-    public Iterable<K> iterator() {
-        List<K> keys = new ArrayList<>();
-        inOrder(root, keys);
-        return keys;
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new InOrderIterator();
     }
 
-    private void inOrder(Node node, List<K> keys) {
-        if (node == null) return;
-        inOrder(node.left, keys);
-        keys.add(node.key);
-        inOrder(node.right, keys);
+    private class InOrderIterator implements Iterator<Entry<K, V>> {
+        private Stack<Node> stack = new Stack<>();
+
+        public InOrderIterator() {
+            pushLeft(root);
+        }
+
+        private void pushLeft(Node node) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public Entry<K, V> next() {
+            Node current = stack.pop();
+            pushLeft(current.right);
+            return new Entry<>(current.key, current.val);
+        }
     }
 }
