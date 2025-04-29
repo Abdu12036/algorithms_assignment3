e283126466356942079f50bff4b82029c73bdf07
Abdunur <amangeldievabdunur@gmail.com>
2025-04-29 22:11:49 +0600

    implemented some methods

--- src/modules/BST.java
+++ src/modules/BST.java
@@ -1,4 +1,54 @@
 package modules;
 
-public class BST {
+import java.util.*;
+
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) return new Node(key, val);
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, val);
+        else if (cmp > 0)
+            node.right = put(node.right, key, val);
+        else
+            node.val = val;
+
+        return node;
+    }
+
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(Node node, K key) {
+        if (node == null) return null;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            return get(node.left, key);
+        else if (cmp > 0)
+            return get(node.right, key);
+        else
+            return node.val;
+    }
+
+
 }
