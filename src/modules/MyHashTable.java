54e101c92ef4074374834075a99a850823dd1147
Abdunur <amangeldievabdunur@gmail.com>
2025-04-29 21:17:32 +0600

    created MyHashTable class

---
+++ src/modules/MyHashTable.java
@@ -0,0 +1,19 @@
+package modules;
+
+public class MyHashTable <K, V>{
+
+    private class HashNode<K, V>{
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+}
